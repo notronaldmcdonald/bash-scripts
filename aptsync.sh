5aa1d399489768cf627358dc46cb1ea3ed0e1567
notronaldmcdonald <78518610+notronaldmcdonald@users.noreply.github.com>
2021-02-04 14:12:37 -0500

    added aptsync.sh
    added aptsync.sh, a quick translation of sync.sh for systems that use apt-get as their package manager

---
+++ aptsync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo This script will install the set of utilities that you use, and update the system.
+read -n1 -r -p "Press any key to start, or CTRL+C to stop execution."
+apt-get upgrade
+echo System update complete.
+read -n1 -r -p "If you wish to stop at just updating the system, press CTRL+C. Otherwise, press any key."
+apt-get install sudo nano neofetch
+echo Basic tools installed. Set up the sudoers file, create a user account, and sign out of root. 
