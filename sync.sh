afa2bc7ed7a566152defbbf64ec2b532dcec7c06
notronaldmcdonald <78518610+notronaldmcdonald@users.noreply.github.com>
2021-02-03 21:59:22 -0500

    uploaded sync.sh

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo This script will install the set of utilities that you use, and update the system.
+read -n1 -r -p "Press any key to start, or CTRL+C to stop execution."
+pacman -Syu --noconfirm
+echo System update complete.
+read -n1 -r -p "If you wish to stop at just updating the system, press CTRL+C. Otherwise, press any key."
+pacman -S --noconfirm sudo nano neofetch
+echo Basic tools installed. Set up the sudoers file, create a user account, and sign out of root. 
