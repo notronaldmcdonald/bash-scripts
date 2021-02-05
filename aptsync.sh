0a9b8983c7c65c0306ecff0780497bba976da40f
notronaldmcdonald <78518610+notronaldmcdonald@users.noreply.github.com>
2021-02-04 22:14:48 -0500

    Update aptsync.sh

--- aptsync.sh
+++ aptsync.sh
@@ -5,5 +5,5 @@ read -n1 -r -p "Press any key to start, or CTRL+C to stop execution."
 apt-get upgrade
 echo System update complete.
 read -n1 -r -p "If you wish to stop at just updating the system, press CTRL+C. Otherwise, press any key."
-apt-get install sudo nano neofetch
+apt-get install nano neofetch
 echo Basic tools installed. Set up the sudoers file, create a user account, and sign out of root. 
