ca7a92e05c416adda7664fc02dccf0efe273da01
brett <78518610+notronaldmcdonald@users.noreply.github.com>
2021-02-13 12:45:33 -0500

    Update aptsync.sh
    marked as officially broken and is therefore no longer counted in the README

--- aptsync.sh
+++ aptsync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# this script is broken. may or may not be fixed. if any of yall adopt a debian-based distro, feel free to fix it yourselves.
 
 echo This script will install the set of utilities that you use, and update the system.
 read -n1 -r -p "Press any key to start, or CTRL+C to stop execution."
@@ -6,4 +7,4 @@ apt-get upgrade
 echo System update complete.
 read -n1 -r -p "If you wish to stop at just updating the system, press CTRL+C. Otherwise, press any key."
 apt-get install nano neofetch
-echo Basic tools installed. Set up the sudoers file, create a user account, and sign out of root. 
+echo Basic tools installed. Create a user account, and sign out of root. 
