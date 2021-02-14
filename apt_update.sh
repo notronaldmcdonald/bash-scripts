b6c5411e0d71c64765b0e1e8a0dfb3a3e08accf0
brett <78518610+notronaldmcdonald@users.noreply.github.com>
2021-02-13 22:24:15 -0500

    added apt_update
    because apt is shit

---
+++ apt_update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Developed by Brett. (https://github.com/notronaldmcdonald)
+# Because apt is shitty.
+apt-get update
+apt-get upgrade
+# seriously tho i cant believe this is a two command process thats actually bad
