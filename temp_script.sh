1ab4640003fbbbeb1a2135789b4a64f2e4de2c88
brett <78518610+notronaldmcdonald@users.noreply.github.com>
2021-02-15 22:01:22 -0500

    Delete temp_script.sh

--- temp_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get install build-essential autoconf2.13 xorg-dev xserver-xorg-dev xutils-dev libtool debhelper
-dpkg --add-architecture i386
-apt-get update
-apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 libc6-i386 lib32tinfo5 libc6-i386
-dpkg -i xf86-input-nextwindow_0.3.4~precise1_amd64.deb
-dpkg -i nwfermi-0.6.5.0_amd64.deb
