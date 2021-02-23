608182d0b3f548321552d7ddf92f3c58f5ccbba8
brett <78518610+notronaldmcdonald@users.noreply.github.com>
2021-02-23 10:55:13 -0500

    updated simple_ssh
    rewrote simple_ssh, added keyfile support, added an additional check to prevent the user from triggering an error by trying to load previous session info without any persistent data file.

--- simple_ssh.sh
+++ simple_ssh.sh
@@ -5,33 +5,89 @@
 # some variables
 
 RESET=$(tput sgr0)
+BOLD=$(tput bold)
 if [ -e .vars ]; then
   source .vars
+fi
+
+# firstrun check
+
+if [ -e .firstrun ]; then
+  source .firstrun
+else
+  echo "${RESET}Creating firstrun file..."
+  touch .firstrun
+  echo "export firstrun=true" > .firstrun
+  echo "Done!"
 fi
 
 # begin script
 
 echo ${RESET}Welcome to SimpleSSH! This script is designed to make using OpenSSH slightly more user friendly.
-echo "Use last session settings? Type YES if yes, or NO for no. (This'll error out if you haven't run the script before.)"
-read answer
-if [ "$answer" != "${answer#[YESyes]}" ]; then
-
-  ssh -p $target_port_persistent $target_user_persistent@$target_ip_persistent
-
-elif [ "$answer" != "${answer#[NOno]}" ]; then
-
-    echo "Okay! Input the user account you're trying to log into remotely."
+echo "Do you use key-based or password based authentication? Type KEY if you use keyfiles, or PASSWD for password."
+read answer_1
+if [ "$answer_1" != "${answer_1#[KEYkey]}" ]; then
+  if [ "$firstrun" = "true" ]; then
+    echo "Would you like to load the previous session settings? Type YES for yes, NO for no."
+    read answer
+    if [ "$answer" != "${answer#[YESyes]}" ]; then
+      ssh -i $keyfile_persistent -p $target_port_persistent $target_user_persistent@$target_ip_persistent
+    elif [ "$answer" != "${answer#[NOno]}" ]; then
+      echo "Okay! First, enter the username of the account you're trying to access. (ex. foo, root)"
+      read target_user && echo "export target_user_persistent=$target_user" > .vars
+      echo "Now, enter the IP address of the computer you're trying to connect to. (If you're on a local network, this will be a local ip address.)"
+      read target_ip && echo "export target_ip_persistent=$target_ip" >> .vars
+      echo "Next, enter the port the SSH daemon is bound to on the target machine. (The default is 22. If you haven't changed it, enter 22.)"
+      read target_port && echo "export target_port_persistent=$target_port" >> .vars
+      echo "Finally, enter the ${BOLD}full directory path ${RESET}of the keyfile."
+      read keyfile && echo "export keyfile_persistent=$keyfile" >> .vars
+      echo "Okay. Connecting with your settings now!"
+      ssh -i $keyfile -p $target_port $target_user@$target_ip
+    else
+      echo Unexpected input received! Exiting... && exit
+    fi
+  elif [ "$firstrun" != "true" ]; then
+    echo "First, enter the username of the account you're trying to access. (ex. foo, root)"
     read target_user && echo "export target_user_persistent=$target_user" > .vars
-    echo "Next, input the IP (local if on LAN) address of the computer you're trying to reach!"
+    echo "Now, enter the IP address of the computer you're trying to connect to. (If you're on a local network, this will be a local ip address.)"
     read target_ip && echo "export target_ip_persistent=$target_ip" >> .vars
-    echo "Finally, input the port the SSH daemon is running on. (Hint: by default, it's 22, so if you haven't changed it, then enter 22.)"
+    echo "Next, enter the port the SSH daemon is bound to on the target machine. (The default is 22. If you haven't changed it, enter 22.)"
     read target_port && echo "export target_port_persistent=$target_port" >> .vars
+    echo "Finally, enter the ${BOLD}full directory path ${RESET}of the keyfile."
+    read keyfile && echo "export keyfile_persistent=$keyfile" >> .vars
+    echo "Okay. Connecting with your settings now!"
+    ssh -i $keyfile -p $target_port $target_user@$target_ip
+  else
+    echo "An unknown error occurred."
+  fi
+elif [ "$answer_1" != "${answer_1#[PASSWDpasswd]}" ]; then
+  if [ "$firstrun" = "true" ]; then
+    echo "Would you like to load the previous session settings? Type YES for yes, NO for no."
+    read answer
+    if [ "$answer" != "${answer#[YESyes]}" ]; then
+      ssh -p $target_port_persistent $target_user_persistent@$target_ip_persistent
+    elif [ "$answer" != "${answer#[NOno]}" ]; then
+      echo "First, enter the username of the account you're trying to access. (ex. foo, root)"
+      read target_user && echo "export target_user_persistent=$target_user" > .vars
+      echo "Now, enter the IP address of the computer you're trying to connect to. (If you're on a local network, this will be a local ip address.)"
+      read target_ip && echo "export target_ip_persistent=$target_ip" >> .vars
+      echo "Finally, enter the port the SSH daemon is bound to on the target machine. (The default is 22. If you haven't changed it, enter 22.)"
+      read target_port && echo "export target_port_persistent=$target_port" >> .vars
+      echo "Okay. Connecting with your settings now!"
+      ssh -p $target_port $target_user@$target_ip
+    else
+      echo Unexpected input received! Exiting... && exit
+    fi
+  elif [ "$firstrun" != "true" ]; then
+    echo "First, enter the username of the account you're trying to access. (ex. foo, root)"
+    read target_user && echo "export target_user_persistent=$target_user" > .vars
+    echo "Now, enter the IP address of the computer you're trying to connect to. (If you're on a local network, this will be a local ip address.)"
+    read target_ip && echo "export target_ip_persistent=$target_ip" >> .vars
+    echo "Finally, enter the port the SSH daemon is bound to on the target machine. (The default is 22. If you haven't changed it, enter 22.)"
+    read target_port && echo "export target_port_persistent=$target_port" >> .vars
+    echo "Okay. Connecting with your settings now!"
     ssh -p $target_port $target_user@$target_ip
-
-else
-
-  echo Unexpected input received!
-
+  fi
 fi
 
 # end script
