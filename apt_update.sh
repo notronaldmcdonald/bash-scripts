a601ab8643faa55312f1b58bfff9a78897aba03e
notronaldmcdonald <mcdonalds.burger.dev@gmail.com>
2021-03-01 11:13:08 -0500

    updated apt_update - restrict to dev until tested

--- apt_update.sh
+++ apt_update.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 # Developed by Brett. (https://github.com/notronaldmcdonald)
 # Because apt is shitty.
-apt-get update
-apt-get upgrade
+
+# variables and functions
+source $HOME/.bashrc
+dir=$(pwd)
+aptupgrade () {
+  apt-get update
+  apt-get upgrade
+}
+
+# begin script
+
+if [ "$add_config" = "false" ]; then
+  read -p "Write as alias 'sysupdate' to user config [y/N]?" answer
+    if [ "$answer" = "y" ]; then
+      echo "alias sysupdate='./${dir}/apt_update.sh'" >> $HOME/.bashrc
+      echo "add_config=true" >> $HOME/.bashrc
+      echo "Appended to .bashrc."
+      aptupgrade
+    else
+      aptupgrade
+    fi
+elif [ "$add_config" = "true" ]; then
+  aptupgrade
+fi
 # seriously tho i cant believe this is a two command process thats actually bad
+
+# end script
